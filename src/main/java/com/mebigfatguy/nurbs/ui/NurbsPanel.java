73ce091bb3e00e01e15e775adebe7f6b894bb7a8
Dave Brosius <dbrosius@mebigfatguy.com>
2019-04-18 00:33:13 -0400

    start drawing the page

--- src/main/java/com/mebigfatguy/nurbs/ui/NurbsPanel.java
+++ src/main/java/com/mebigfatguy/nurbs/ui/NurbsPanel.java
@@ -20,27 +20,47 @@ package com.mebigfatguy.nurbs.ui;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Point;
 
 import javax.swing.JPanel;
 
 public class NurbsPanel extends JPanel {
 
-	private Point pageSize;
+    private static final int BORDER_SIZE = 20;
 
-	public NurbsPanel() {
-		pageSize = new Point(1000, 1000);
-	}
+    private double scaleFactor;
+    private Dimension pageSize;
 
-	@Override
-	public void paintComponent(Graphics g) {
-		Graphics render = g.create();
-		try {
-			render.setColor(Color.GRAY);
-			Dimension sz = getSize();
-			render.fillRect(0, 0, sz.width, sz.height);
-		} finally {
-			render.dispose();
-		}
-	}
+    public NurbsPanel() {
+        scaleFactor = 1.0;
+        pageSize = new Dimension(1000, 1000);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        Graphics render = g.create();
+        try {
+            render.setColor(Color.GRAY);
+            Dimension sz = getSize();
+            render.fillRect(0, 0, sz.width, sz.height);
+
+            // need to scale the page
+            render.setColor(Color.WHITE);
+            render.fillRect(BORDER_SIZE, BORDER_SIZE, pageSize.width, pageSize.height);
+            render.setColor(Color.BLACK);
+            render.drawRect(BORDER_SIZE, BORDER_SIZE, pageSize.width, pageSize.height);
+
+        } finally {
+            render.dispose();
+        }
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        Dimension sz = (Dimension) pageSize.clone();
+        sz.width += 2 * BORDER_SIZE;
+        sz.height += 2 * BORDER_SIZE;
+        sz.width *= scaleFactor;
+        sz.height *= scaleFactor;
+        return sz;
+    }
 }
