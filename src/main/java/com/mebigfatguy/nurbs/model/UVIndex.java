d1c54da50ae3f542efd4131bbe15448b94b01c65
Dave Brosius <dbrosius@mebigfatguy.com>
2019-05-05 20:40:44 -0400

    add UVIndex class

---
+++ src/main/java/com/mebigfatguy/nurbs/model/UVIndex.java
@@ -0,0 +1,58 @@
+/*
+ * nurbs - a playground for messing around with nurbs
+ * Copyright 2019 MeBigFatGuy.com
+ * Copyright 2019 Dave Brosius
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.mebigfatguy.nurbs.model;
+
+public class UVIndex {
+
+    private int uIndex;
+    private int vIndex;
+
+    public UVIndex(int u, int v) {
+        uIndex = u;
+        vIndex = v;
+    }
+
+    public int getU() {
+        return uIndex;
+    }
+
+    public int getV() {
+        return vIndex;
+    }
+
+    @Override
+    public int hashCode() {
+        return uIndex << 16 | vIndex;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof UVIndex)) {
+            return false;
+        }
+
+        UVIndex that = (UVIndex) o;
+
+        return uIndex == that.uIndex && vIndex == that.vIndex;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + uIndex + "," + vIndex + ")";
+    }
+}
